ee93624d6afd763ff2bf2905aacb20b39480b754
Unknown <craigdennis1990@gmail.com>
2020-03-22 22:36:57 +0000

    unwanted deprecated code
    remove old and deprecate code from app.js

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ var cross_slide, ns_slide;
 //Specify pixels gap between each slideshow rotation (use integer):
 var slideshowgap = 50;
 var actualwidth = '';
-var iedom = document.getElementById;
-
 
 
 function fillup() {
-    if (iedom) {
       cross_slide = document.getElementById
         ? document.getElementById('test2')
         : document.all.test2;
@@ -20,48 +17,22 @@ function fillup() {
         ? cross_slide.offsetWidth
         : document.getElementById('temp').offsetWidth;
       cross_slide2.style.left = actualwidth + slideshowgap + 'px';
-    } else if (document.layers) {
-      ns_slide = document.ns_slidemenu.document.ns_slidemenu2;
-      ns_slide2 = document.ns_slidemenu.document.ns_slidemenu3;
-      ns_slide.document.write(leftrightslide);
-      ns_slide.document.close();
-      actualwidth = ns_slide.document.width;
-      ns_slide2.left = actualwidth + slideshowgap;
-      ns_slide2.document.write(leftrightslide);
-      ns_slide2.document.close();
-    }
+
     lefttime = setInterval('slideleft()', 30);
   }
   window.onload = fillup;
 
   function slideleft() {
-    if (iedom) {
       if (parseInt(cross_slide.style.left) > actualwidth * -1 + 8)
         cross_slide.style.left =
           parseInt(cross_slide.style.left) - copyspeed + 'px';
-      else
+      else {
         cross_slide.style.left =
           parseInt(cross_slide2.style.left) +
           actualwidth +
           slideshowgap +
           'px';
-
-      if (parseInt(cross_slide2.style.left) > actualwidth * -1 + 8)
-        cross_slide2.style.left =
-          parseInt(cross_slide2.style.left) - copyspeed + 'px';
-      else
-        cross_slide2.style.left =
-          parseInt(cross_slide.style.left) +
-          actualwidth +
-          slideshowgap +
-          'px';
-    } else if (document.layers) {
-      if (ns_slide.left > actualwidth * -1 + 8)
-        ns_slide.left -= copyspeed;
-      else ns_slide.left = ns_slide2.left + actualwidth + slideshowgap;
-
-      if (ns_slide2.left > actualwidth * -1 + 8)
-        ns_slide2.left -= copyspeed;
-      else ns_slide2.left = ns_slide.left + actualwidth + slideshowgap;
-    }
+      }  
   }
+
+
